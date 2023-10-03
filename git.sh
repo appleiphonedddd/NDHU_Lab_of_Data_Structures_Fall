64fdc2d3fbf359018e830324f25a5e19b71b7e30
appleiphonedddd <U08157112@o365.mcut.edu.tw>
2023-10-03 20:10:42 +0800

    upgrade

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+repository_url="https://github.com/appleiphonedddd/NDHU_Lab_of_Data_Structures_2023.git"
+branch="main"
+commit_message="upgrade"
+
+
+git add .
+
+git commit -m "$commit_message"
+
+git pull origin $branch
+
+git push origin $branch
+
+echo "Pushed changes to $repository_url on branch $branch."
