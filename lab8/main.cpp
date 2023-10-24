04bb1a1166e141fe06176f7214c75b9219da9b26
李致彬 <89340462+appleiphonedddd@users.noreply.github.com>
2023-10-24 19:12:35 +0800

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -109,13 +109,9 @@ public:
     The gird in left top is start point and right bottom is finish point.
     Randomly generate 20% wall in the maze.
     Make sure start point and finish point are 0
-
-    動態配置一個二維的鏈結串列，大小是 s * s，用這個鏈結串列表示迷宮的地圖
-    節點中 0 的值表示可以移動的空間， 1 表示牆壁
-    左上角的位置表示起點，右下角的位置表示終點
-    請在迷宮中加入 20% 的牆壁
-    然後確定起點跟終點都是可以移動的位置
     */
+
+
     void initMaze(int s)
     {
         maze = new Grid();
@@ -159,10 +155,8 @@ public:
     Return a list content the path information inside.
     If there is no path between two point then the list will be empty.
 
-    這個函數會找到起點到終點間的一條路徑
-    回傳一個 list 包含著路徑的資訊
-    如果找不到路徑的話 list 就會是空的
-    */
+
+    
     bool findPath(Grid *start, Grid *end, List &path)
     {
         if (start == nullptr || start->getState() == 1)
