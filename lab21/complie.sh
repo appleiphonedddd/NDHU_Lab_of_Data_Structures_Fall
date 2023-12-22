bb79af40c1a3b8ec30df9ef7e4641f87416e4c2b
appleiphonedddd <611221201@gms.ndhu.edu.tw>
2023-12-22 22:41:55 +0800

    write_complie_script

---
+++ lab21/complie.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if the user provided a C source file as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <source_file.c>"
+    exit 1
+fi
+
+# Get the name of the source file without the extension
+source_file="$1"
+file_name="${source_file%.*}"
+
+# Compile the C code to create an executable
+gcc -o "$file_name" "$source_file"
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Executing '$file_name'..."
+    
+    # Execute the compiled binary
+    "./$file_name"
+    
+    # Check if execution was successful
+    if [ $? -eq 0 ]; then
+        echo "Execution successful. Removing '$file_name'..."
+        
+        # Remove the compiled binary
+        rm "$file_name"
+        
+        echo "'$file_name' removed."
+    else
+        echo "Execution failed."
+    fi
+else
+    echo "Compilation failed."
+fi
